e4aa9d6cbfb47f9abdb714b34714fdc57081d743
Bilguun <soproatlol@gmail.com>
2026-04-12 01:31:45 +1000

    Tree bark image is now proper

--- js/entities/TreeBackground.js
+++ js/entities/TreeBackground.js
@@ -1,12 +1,13 @@
 const sharedSprite = new Image();
-sharedSprite.src = 'assets/art/treeBark.png';
+sharedSprite.src = 'assets/art/treeBarkSmall.png';
 
 // List of 3 possible trees/background tiles to randomly choose from
 const spriteVariations = [
-    // { sx: 18 * 20 + 1, sy: 18 * 4 + 1, sWidth: 16, sHeight: 16 }, // Tile 1 (Original)
-    // { sx: 18 * 20 + 1, sy: 18 * 5 + 1, sWidth: 16, sHeight: 16 }, // Tile 2 (Customize here)
-    // { sx: 18 * 20 + 1, sy: 18 * 6 + 1, sWidth: 16, sHeight: 16 }  // Tile 3 (Customize here)
-    { sx: 0, sy: 0, sWidth: 1000, sHeight: 1000 }, // Tile 1 (Original)
+    { sx: 18 * 0, sy: 18 * 0, sWidth: 9, sHeight: 9 }, // Tile 1 (Original) 
+    { sx: 18 * 1, sy: 18 * 0, sWidth: 9, sHeight: 9 }, // Tile 1 (Original)
+    { sx: 18 * 2, sy: 18 * 0, sWidth: 9, sHeight: 9 }, // Tile 1 (Original)
+    { sx: 18 * 3, sy: 18 * 0, sWidth: 9, sHeight: 9 }, // Tile 1 (Original)
+    { sx: 18 * 0, sy: 18 * 1, sWidth: 9, sHeight: 9 }, // Tile 1 (Original)
 ];
 
 export class TreeBackground {
